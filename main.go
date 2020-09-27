4d5cd456cf1f01062d3a4becd72d870906d4ab92
yuxinburen <861574834@qq.com>
2020-09-27 20:56:18 +0800

    存储数据到数据库,使用hash算法对信息进行脱敏

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"BeegoProject0603/db_mysql"
+	_ "BeegoProject0603/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	//1、连接数据库
+	db_mysql.Connect()
+
+	//2、其他配置
+	
+	//3、启动应用
+	beego.Run()//代码简洁
+}
+
---
+++ routers/router.go
@@ -0,0 +1,13 @@
+package routers
+
+import (
+	"BeegoProject0603/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	//用户注册功能接口 http://127.0.0.1:8080/register
+	beego.Router("/register",&controllers.RegisterController{})
+	//http://127.0.0.1:8080
+    beego.Router("/", &controllers.MainController{})
+}
