97a72c472346af10dd3bf4ae9213dcf40a9d7a20
unknown <1084146423@qq.com>
2020-09-29 19:13:46 +0800

    JSON

--- controllers/registerController.go
+++ controllers/registerController.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
-	"BeegoProject0603/db_mysql"
-	"BeegoProject0603/models"
+	"HelloBeego0604/dbmysql"
+	"HelloBeego0604/models"
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego"
@@ -13,26 +15,50 @@ type RegisterController struct {
 	beego.Controller
 }
 
-func (r *RegisterController) Post(){
-	//解析前端提交的用户注册的信息
-	dataBytes, err := ioutil.ReadAll(r.Ctx.Request.Body)
-	if err != nil {
-		r.Ctx.WriteString("数据接收错误，请重试")
+func (r *RegisterController) Post() {
+	//解析前端提交的用户注册信息
+	dataBytes,err :=ioutil.ReadAll(r.Ctx.Request.Body)
+	if err != nil{
+		r.Ctx.WriteString("数据接错误，请重试")
 		return
 	}
 	var user models.User
-	err = json.Unmarshal(dataBytes, &user)
-	if err != nil {
+	err =json.Unmarshal(dataBytes,&user)
+	if err != nil{
 		r.Ctx.WriteString("数据解析错误，请重试")
+		result := models.Result{
+			Code: 0,
+			Message: "数据解析错误，请重试",
+			Data: nil,
+		}
+		r.Data["json"] = &result
 		return
 	}
-	//一切正常，将用户信息保存到数据库中取
-	//直接调用保存数据的一个函数,并判断保存后的结果
-	row, err :=db_mysql.AddUser(user)
-	if err != nil {
-		r.Ctx.WriteString("注册用户信息失败，请重试")
-		return
+	//一切正常，将用户信息保存到数据库中
+	//直接调用保存数据的一个函数，并判断保存后的结果
+	row, err := dbmysql.AddUser(user)
+	if err != nil{
+		//r.Ctx.WriteString("注册用户信息失败，请重试")
+		result := models.Result{
+			Code: 0,
+			Message: "注册用户信息失败,请重试",
+			Data: nil,
+		}
+		r.Data["json"] = &result
 	}
 	fmt.Println(row)
-	r.Ctx.WriteString("恭喜，注册用户信息成功")
+
+	md5Hash := md5.New()
+	md5Hash.Write([]byte(user.Password))
+	user.Password =hex.EncodeToString(md5Hash.Sum(nil))
+	result := models.Result{
+		Code: 1,
+		Message: "恭喜,注册用户信息成功",
+		Data: user,
+	}
+	//json.Marshal()
+	r.Data["json"] = &result
+	r.ServeJSON()//将result编码为json格式返回给前端
+	//r.Ctx.WriteString("恭喜,注册用户信息成功")
+
 }
